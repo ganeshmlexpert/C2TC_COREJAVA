790e7a72be4ebc86a2d3c888b7d787a660619a97
Ganesh <Ganesh@DESKTOP-ALJQD7V>
2024-11-07 05:55:09 -0800

    second

---
+++ C2TC_CORE_JAVA/src/com/tns/ifet/day1/second.java
@@ -0,0 +1,10 @@
+package com.tns.ifet.day1;
+
+public class second {
+	public static void main(String[] args) {
+		int number=20;
+		System.out.println("value is:"+number);
+		String student="Gokul";
+		System.out.println("name:"+student);
+	}
+}

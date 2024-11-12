4bd917f33992b34d1619ec45aaf294770d0b0154
Ganesh <Ganesh@DESKTOP-ALJQD7V>
2024-11-12 07:13:16 -0800

    3rd

--- C2TC_CORE_JAVA/src/com/tns/ifet/dayone/IfElseDemo.java
+++ C2TC_CORE_JAVA/src/com/tns/ifet/dayone/IfElseDemo.java
@@ -1,12 +1,16 @@
-package com.tns.ifet.day1;
+package com.tns.ifet.dayone;
 
 public class IfElseDemo {
+
 	public static void main(String[] args) {
-		int age=21;
+		// TODO Auto-generated method stub
+		int age=11;
 		if(age>=18)
 		System.out.println(" You are eligible for Voting");
 		else
 		System.out.println("Sorry! You need to wait more");
 		System.out.println("if else demonstration");
-		}
+
+	}
+
 }
